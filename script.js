45e917c792e93e319db1b938863424a920ea9b04
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2021-03-17 13:51:46 -0700

    Fixed elapsed time

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ var loop = setInterval(function() {
         $("#timeRemaining").text(timeTill);
         console.log("New Remaining Time");
     }
-    if ($("#timeElapsed").text()) {
+    if ($("#timeElapsed").text() != timeElapsed) {
         $("#timeElapsed").text(timeElapsed);
         console.log("New Elapsed Time");
     }
